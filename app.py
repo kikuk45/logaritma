f33a5588b9d1d9310b5db14a05ee58e8a32a70a6
kikuk45 <m.rifqialkhadziq1313@gmail.com>
2026-09-15 18:20:26 +0700

    app.py

--- app.py
+++ app.py
@@ -11,12 +11,12 @@ st.set_page_config(
     layout="wide",
 )
 
-# Custom CSS Adaptif (Satu String Utuh)
+# Custom CSS Adaptif
 custom_css = """
 
 """
 
 st.markdown(custom_css, unsafe_allow_html=True)
 
-# Header Utama
+# Header Utama (Ditulis dalam 1 baris utuh)
 st.markdown('
