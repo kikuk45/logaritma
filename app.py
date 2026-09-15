7c1d16e1930b8dbf1f524d8e8b790d52c8b97f03
kikuk45 <m.rifqialkhadziq1313@gmail.com>
2026-09-15 21:34:27 +0700

    app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ st.set_page_config(
 )
 
 # -----------------------------------------------------------------------------
-# CUSTOM CSS - TEMA KARTUN PENDIDIKAN & PAKSA LIGHT MODE
+# CUSTOM CSS TUNGGAL - PAKSA LIGHT MODE & GAYA KARTUN EDUKASI
 # -----------------------------------------------------------------------------
 st.markdown("""
 
